ae62106acabdb7cda5fddd3e61fcf274a435327a
djdcann <djdcann@gmail.com>
2018-01-18 14:02:33 -0500

    update

--- postgresql/db.py
+++ postgresql/db.py
@@ -2,21 +2,12 @@ import psycopg2
 import secrets
 from psycopg2 import sql
 
-try:
-    conn = psycopg2.connect("dbname='postgres' user='pygres' host='localhost' password='%s'" % secrets.db_pass)
-    cur = conn.cursor()
-    cur.execute(
-        sql.SQL("insert into {} values (%s, %s)").format(sql.Identifier('fierce_followers')), [12345, 'hello'])
-    cur.execute("select * from fierce_followers")
-    rows = cur.fetchall()
-    print rows
-    conn.commit()
-    cur.close()
-except Exception as e:
-    print e.message
-
 
 class PostgresDB:
+
+    default_dbname = "postgres"
+    default_user = "pygres"
+    default_host = "localhost"
 
     def __init__(self):
         self.conn = self.open(pw=secrets.db_pass)
@@ -31,8 +22,24 @@ class PostgresDB:
 
     #open a connection to PostgreSQL DB
     @staticmethod
-    def open(dbname="postgres", user="pygres", host="localhost", pw=None):
+    def open(dbname=default_dbname, user=default_user, host=default_host, pw=None):
         return psycopg2.connect("dbname='%s' user='%s' host='%s' password='%s'" % (dbname, user, host, pw))
 
     def __del__(self):
         self.conn.close()
+
+
+try:
+    conn = PostgresDB.open(pw=secrets.db_pass)
+    cur = conn.cursor()
+    cur.execute(
+        sql.SQL("insert into {} values (%s, %s)").format(sql.Identifier('fierce_followers')), [12345, 'hello'])
+    cur.execute("select * from fierce_followers")
+    rows = cur.fetchall()
+    print rows
+    conn.commit()
+    cur.close()
+except Exception as e:
+    print e.message
+
+
